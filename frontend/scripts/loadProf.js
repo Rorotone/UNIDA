6ff96af723146c7d686092fde3f28a8cd9d99715
Diego-H <d.herreramolina@uandresbello.edu>
2025-09-08 23:25:52 -0300

    test_1

--- frontend/scripts/loadProf.js
+++ frontend/scripts/loadProf.js
@@ -217,4 +217,3 @@ async function eliminarProfesor(id_profesor) {
 // Asegúrate de que estas funciones estén disponibles globalmente
 window.editarProfesor = editarProfesor;
 window.eliminarProfesor = eliminarProfesor;
-console.error('Error al eliminar profesor:', error);
